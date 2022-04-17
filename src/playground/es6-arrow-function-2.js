d39a582b548a138a0c3e82d9b36bdb7217466d31
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-17 00:12:27 -0700

    Add 'this' keyword example

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,6 +1,30 @@
-const add = function(a, b) {
-  console.log(arguments);
-  return a+b;
+// arguments object - no longer bound with arrow functions
+
+// const add = (a, b) => {
+//   console.log(arguments);
+//   return a+b;
+// }
+
+// console.log(add(55, 1));
+
+// this keyword - no longer bound with arrow functions
+
+const user = {
+  name: 'Andrew',
+  cities: ['Philadelphia', 'New York', 'Dublin'],
+  printPlacesLived: function () {
+    console.log(this.name);
+    console.log(this.cities);
+    // doesn't work:
+    // this.cities.forEach(function(city) {
+    //   console.log(this.name + ' has lived in ' + city);
+    // });
+
+    // works with arrow here:
+    this.cities.forEach((city) => {
+      console.log(this.name + ' has lived in ' + city);
+    }
+    )}
 }
 
-console.log(add(55, 1));
+user.printPlacesLived();
