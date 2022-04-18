7c709867a11c8d540d842a3a1769394e884b8101
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-18 13:46:35 -0700

    Use map method to multiply numbers

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -17,3 +17,13 @@ const user = {
   }
 };
 console.log(user.printPlacesLived());
+
+const multiplier = {
+  numbers: [1,2,3],
+  multiplyBy: 2,
+  multiply() {
+    return this.numbers.map((num) => num * this.multiplyBy);
+  }
+}
+
+console.log(multiplier.multiply());
