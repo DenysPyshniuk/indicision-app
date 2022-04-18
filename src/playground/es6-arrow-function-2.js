ee7be7a71be7280225bf1cded3c3400093f7561c
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-18 13:39:49 -0700

    Use es6 syntax to create a method in the object

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -12,19 +12,8 @@
 const user = {
   name: 'Andrew',
   cities: ['Philadelphia', 'New York', 'Dublin'],
-  printPlacesLived: function () {
-    console.log(this.name);
-    console.log(this.cities);
-    // doesn't work:
-    // this.cities.forEach(function(city) {
-    //   console.log(this.name + ' has lived in ' + city);
-    // });
-
-    // works with arrow here:
-    this.cities.forEach((city) => {
-      console.log(this.name + ' has lived in ' + city);
-    }
-    )}
-}
-
-user.printPlacesLived();
+  printPlacesLived() {
+    return this.cities.map((city) => this.name + ' has lived in ' + city);
+  }
+};
+console.log(user.printPlacesLived());
