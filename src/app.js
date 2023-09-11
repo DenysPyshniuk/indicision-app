a00294a602e0e1a5ccf68c46fb09f65764e0b555
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2023-09-11 09:53:28 -0700

    Implement onFormSubmit in order to show how many options we have submitted to the form

--- src/app.js
+++ src/app.js
@@ -4,30 +4,42 @@ console.log('App is running');
 
 const app = {
   title: 'Indecision App',
-  subtitle: 'This is some info'
-}
-
+  subtitle: 'This is some info',
+  options: []
+};
 
 const onFormSubmit = (e) => {
   e.preventDefault();
-  console.log('Form submitted!')
-}
-
-
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-    <form onSubmit={onFormSubmit}>
-      <input type="text" name="option"/>
-      <button>Add Option</button>
-    </form>
-  </div>
-);
+  const option = e.target.elements.option.value;
+  // console.log(option.target.elements.option.value);
+  if(option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    console.log('options', app.options);
+    renderFormApp();
+  }
+};
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+const renderFormApp = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? `You have ${app.options.length} options.` : "You don't have any options yet"}</p>
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"/>
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+  };
+
+  renderFormApp();
