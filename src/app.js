feabf90b1dd42f2415e897753b32655953d96112
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2023-09-11 10:11:33 -0700

    Add remove options button

--- src/app.js
+++ src/app.js
@@ -1,45 +1,55 @@
-console.log('App is running');
+console.log("App is running");
 
 // JSX - JavaScript XML
 
 const app = {
-  title: 'Indecision App',
-  subtitle: 'This is some info',
-  options: []
+  title: "Indecision App",
+  subtitle: "Add your options here:",
+  options: [],
 };
 
 const onFormSubmit = (e) => {
   e.preventDefault();
   const option = e.target.elements.option.value;
   // console.log(option.target.elements.option.value);
-  if(option) {
+  if (option) {
     app.options.push(option);
-    e.target.elements.option.value = '';
-    console.log('options', app.options);
+    e.target.elements.option.value = "";
+    console.log("options", app.options);
     renderFormApp();
   }
 };
 
-const appRoot = document.getElementById('app');
+const removeOptions = () => {
+  app.options = [];
+  console.log("options", app.options);
+  renderFormApp();
+}
+
+const appRoot = document.getElementById("app");
 
 const renderFormApp = () => {
   const template = (
     <div>
       <h1>{app.title}</h1>
       {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? `You have ${app.options.length} options.` : "You don't have any options yet"}</p>
-      <ol>
-        <li>Item one</li>
-        <li>Item two</li>
-      </ol>
       <form onSubmit={onFormSubmit}>
-        <input type="text" name="option"/>
+        <input type="text" name="option" />
         <button>Add Option</button>
       </form>
+      <p>
+        {app.options.length > 0
+          ? `You have ${app.options.length} options.`
+          : "You don't have any options yet"}
+      </p>
+      <ol>
+        <li>options...</li>
+      </ol>
+      <button onClick={removeOptions}>Remove all options</button>
     </div>
   );
 
   ReactDOM.render(template, appRoot);
-  };
+};
 
-  renderFormApp();
+renderFormApp();
