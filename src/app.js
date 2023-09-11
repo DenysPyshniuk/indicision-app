7256c3e9095a11fe23ac1d4f9f47ecb713e07c40
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2023-09-11 10:52:15 -0700

    Clean up and add button makeDecision

--- src/app.js
+++ src/app.js
@@ -11,18 +11,21 @@ const app = {
 const onFormSubmit = (e) => {
   e.preventDefault();
   const option = e.target.elements.option.value;
-  // console.log(option.target.elements.option.value);
   if (option) {
     app.options.push(option);
     e.target.elements.option.value = "";
-    console.log("options", app.options);
     renderFormApp();
   }
 };
 
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+}
+
 const removeOptions = () => {
   app.options = [];
-  console.log("options", app.options);
   renderFormApp();
 }
 
@@ -33,15 +36,13 @@ const renderFormApp = () => {
     <div>
       <h1>{app.title}</h1>
       {app.subtitle && <p>{app.subtitle}</p>}
+
+      <button onClick={onMakeDecision}>What should I do?</button>
+
       <form onSubmit={onFormSubmit}>
         <input type="text" name="option" />
         <button>Add Option</button>
       </form>
-      <p>
-        {app.options.length > 0
-          ? `You have ${app.options.length} options.`
-          : "You don't have any options yet"}
-      </p>
       <ol>
         {
           app.options.map(
