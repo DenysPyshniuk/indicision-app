7dd1fd79f6acb3537616a30eb846b278e72babdb
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2023-09-11 18:57:24 -0700

    Clean up counter app

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,7 +1,6 @@
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    //this.handler = this.handler.bind(this);
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleAddOne = this.handleAddOne.bind(this);
     this.handleResetButton = this.handleResetButton.bind(this);
@@ -29,9 +28,9 @@ class Counter extends React.Component {
   handleResetButton() {
     this.setState(() => {
       return {
-        count: 0
-      }
-    })
+        count: 0,
+      };
+    });
   }
 
   render() {
@@ -47,37 +46,3 @@ class Counter extends React.Component {
 }
 
 ReactDOM.render(<Counter />, document.getElementById("app"));
-
-// let count = 0;
-
-// const addOne = () => {
-//   count ++;
-//   renderCounterApp();
-// }
-
-// const minusOne = () => {
-//   count --;
-//   renderCounterApp();
-// }
-
-// const reset = () => {
-//   count = 0;
-//   renderCounterApp();
-// }
-
-// const appRoot = document.getElementById('app');
-
-// const renderCounterApp = () => {
-//   const template = (
-//     <div>
-//       <h1>Count: {count}</h1>
-//       <button onClick={minusOne}>-1</button>
-//       <button onClick={reset}>RESET</button>
-//       <button onClick={addOne}>+1</button>
-//     </div>
-//   );
-
-//   ReactDOM.render(template, appRoot);
-// }
-
-// renderCounterApp();
