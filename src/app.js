f3e0f95aa43ee40527a4e6841d1abac6e03349f6
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2023-09-20 09:57:30 -0700

    Convert class components to functional

--- src/app.js
+++ src/app.js
@@ -60,50 +60,42 @@ class IndecisionApp extends React.Component {
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-      </div>
-    );
-  }
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  )
 }
 
-class Action extends React.Component {
-  render() {
+const Action = (props) => {
     return (
       <div>
         <button
-          onClick={this.props.handlePick}
-          disabled={!this.props.hasOptions}
+          onClick={props.handlePick}
+          disabled={!props.hasOptions}
         >
           What should I do?
         </button>
       </div>
     );
-  }
 }
 
-class Options extends React.Component {
-  render() {
+const Options = (props) => {
     return (
       <div>
         <ol>
-          {this.props.options.map((option) => (
+          {props.options.map((option) => (
             <Option key={option} optionText={option} />
           ))}
         </ol>
-        <button onClick={this.props.handleRemoveAll}>Remove all options</button>
+        <button onClick={props.handleRemoveAll}>Remove all options</button>
       </div>
     );
-  }
 }
 
-class Option extends React.Component {
-  render() {
-    return <li key={this.props.option}>{this.props.optionText}</li>;
-  }
+const Option = () => {
+    return <li key={props.option}>{props.optionText}</li>;
 }
 
 class AddOption extends React.Component {
